52f6be1f737d1c83ee3174895f5d643b9a207019
Arunp112 <arunp979589@gmail.com>
2025-04-16 02:24:08 +0530

    modify alignment

--- src/components/ProductDetail.jsx
+++ src/components/ProductDetail.jsx
@@ -20,38 +20,35 @@ const ProductDetail = () => {
     );
 
   return (
-    <div className="min-h-screen">
-      <div className=" p-4 dark:bg-gray-900 dark:text-white justify-center ">
-        <div className="">
-          <Link to="/" className="text-blue-500">
-            <div className="flex justify-between   items-center gap-2">
-              <div>
-
+    <div className="min-h-screen p-4 dark:bg-gray-900 dark:text-white justify-center ">
+      <div className="">
+        <Link to="/" className="text-blue-500">
+          <div className="flex justify-between   items-center gap-2">
+            <div className="flex justify-center   items-center gap-2">
               <span className="text-2xl">←</span> <span>Back</span>
-              </div>
-              <div></div>
             </div>
-          </Link>
-        </div>
-        <div className="flex justify-center m-auto items-center">
-          <div className="flex flex-col gap-6 mt-4 mx-auto">
-            <img
-              src={product.image}
-              alt={product.title}
-              className="w-80 h-80 object-contain"
-            />
-            <div>
-              <h2 className="text-2xl font-bold">{product.title}</h2>
-              <p className="text-sm text-gray-500 dark:text-gray-300">
-                {product.category}
-              </p>
-              <p className="text-xl font-semibold text-blue-600">
-                ${product.price}
-              </p>
-              <p className="mt-4">{product.description}</p>
-              <div className="mt-2 text-yellow-500">
-                ⭐ {product.rating?.rate} ({product.rating?.count})
-              </div>
+            <div></div>
+          </div>
+        </Link>
+      </div>
+      <div className="flex justify-center m-auto sm:w-[500px] items-center">
+        <div className="flex flex-col gap-6 mt-4 mx-auto">
+          <img
+            src={product.image}
+            alt={product.title}
+            className="w-80 h-80 object-contain"
+          />
+          <div>
+            <h2 className="text-2xl font-bold">{product.title}</h2>
+            <p className="text-sm text-gray-500 dark:text-gray-300">
+              {product.category}
+            </p>
+            <p className="text-xl font-semibold text-blue-600">
+              ${product.price}
+            </p>
+            <p className="mt-4">{product.description}</p>
+            <div className="mt-2 text-yellow-500">
+              ⭐ {product.rating?.rate} ({product.rating?.count})
             </div>
           </div>
         </div>
